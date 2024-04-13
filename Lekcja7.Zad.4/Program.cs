6b090a459d92684e7c1350fc6ccd088ff7010256
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 06:58:04 +0200

    Add project files.

---
+++ Lekcja7.Zad.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Podaj rok: ");
+
+		// Pobranie roku od użytkownika i konwersja na typ int
+		int rok = Convert.ToInt32(Console.ReadLine());
+
+		// Sprawdzenie warunków dla roku przestępnego
+		if ((rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0))
+		{
+			Console.WriteLine($"{rok} jest rokiem przestępnym.");
+		}
+		else
+		{
+			Console.WriteLine($"{rok} nie jest rokiem przestępnym.");
+		}
+
+		Console.ReadLine();
+	}
+}
